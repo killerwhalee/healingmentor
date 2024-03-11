e1a1e80e64a73ccd069e952a94d27cef7763eecf
killerwhalee <killerwhalee@kaist.ac.kr>
2024-03-11 17:32:19 +0900

    Set default profile image

--- common/models.py
+++ common/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-#from group.models import UserGroup
+# from group.models import UserGroup
 
 """
 Extended User Profile Model
@@ -16,8 +16,10 @@ Isolated from login information, it seems more secured.
 # User Profile Model
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField("User Profile Image", null=True)
-    #group = models.ForeignKey(UserGroup, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(
+        "User Profile Image",
+        default="/static/images/profile-default.png",
+    )
 
 
 @receiver(post_save, sender=User)
