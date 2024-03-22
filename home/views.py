cb5692380de30a9f3f74b899535edeac36433a03
killerwhalee <killerwhalee@kaist.ac.kr>
2024-03-22 11:43:15 +0000

    Created contact-us page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from board.models import Post
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    notice_list = Post.objects.filter(category="notice").order_by("-date_created")[:5]
+
+    context = {"notice_list": notice_list}
+    return render(request, "home/index.html", context=context)
+
+def contact_us(request):
+    return render(request, "home/contact-us.html")
